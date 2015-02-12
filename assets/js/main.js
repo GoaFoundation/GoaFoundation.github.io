a10524948ba65baed5bb9297aebd18f983b7358f
milindalvares <milind@hashcooki.es>
2015-02-13 05:01:12 +0530

    Updating

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,14 @@
-var $blogContent = $('.content');
-var $header = $('.header');
-var $socialIcons = $('.social-icons');
-var $contentTransition = $('.content , .header ');
-var path = window.location.pathname;
+(function($){})(window.jQuery);
+
 
 $(document).ready(function() {
+	var winH = $(window).height();
+		
+	$('#header').css({
+		height: winH
+	});
 	
-	if ( path.match('/diary/post') || path.match('/pil/') )
-	{
-		
-		$header.css({
-			'padding-top':'80px'
-		});
-		$socialIcons.css({
-			'padding': '0px 30px' ,
-			'font-size': '12px'
-		});
-		$blogContent.addClass('col-md-10');
-		$blogContent.removeClass('col-md-8');
-		$header.addClass('col-md-2');
-		$header.removeClass('col-md-4');
-		
-	}
+	$('.content').css({
+		minHeight: winH
+	});
 });
